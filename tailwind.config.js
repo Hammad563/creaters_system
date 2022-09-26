014ade5211532fd674e524f61d0eaf93b7178133
Hammad <61918235+Hammad563@users.noreply.github.com>
2022-09-26 19:16:19 -0400

    devise user controls and landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+  theme: {
+    extend: {
+      colors: {
+        teal: colors.teal,
+        cyan: colors.cyan,
+        white: colors.white,
+      }
+    }
+  },
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
+    './app/javascript/**/*.js',
   ]
 }
